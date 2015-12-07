6bd9f42deda76ceb79da7d296aa6caef37b03812
Tina Schönborn <tina.schoenborn@student.htw-berlin.de>
2015-12-07 17:38:05 +0100

    Add one hour cache

--- aufgabe4/weatherin/app/models/weather.rb
+++ aufgabe4/weatherin/app/models/weather.rb
@@ -1,7 +1,7 @@
 class Weather < ActiveRecord::Base
   attr_accessor :city, :value, :temperature
 
-  def self.search(search)
-    where('city LIKE ?', "%#{search}").first
+  def self.search(cityname)
+    where({ city: cityname, created_at: (Time.now - 1.hour)..Time.now }).first
   end
 end
