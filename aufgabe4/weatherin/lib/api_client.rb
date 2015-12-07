df05e19af0490ab0351de83855ac17246a61cc51
Tina Schönborn <tina.schoenborn@student.htw-berlin.de>
2015-12-07 17:36:38 +0100

    Sanitize user input

--- aufgabe4/weatherin/lib/api_client.rb
+++ aufgabe4/weatherin/lib/api_client.rb
@@ -4,7 +4,8 @@ require 'uri'
 class ApiClient
 
   def self.getweather(cityname)
-    uri = URI.parse("http://api.openweathermap.org/data/2.5/weather?q=#{cityname}&appid=#{ENV['WEATHERAPI_KEY']}")
+    city = cityname.gsub(/\s+/, "")
+    uri = URI.parse("http://api.openweathermap.org/data/2.5/weather?q=#{city}&appid=#{ENV['WEATHERAPI_KEY']}")
     response = Net::HTTP.get_response(uri) 
     response.body   
   end
